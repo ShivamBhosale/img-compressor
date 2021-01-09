e3c32975ac29474e8fccb10f8143d301fef94721
Shivam Bhosale <shivambhosale1511@gmail.com>
2021-01-10 00:54:29 +0530

    Add files via upload

---
+++ Image_Compressor2.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image 
+
+
+st.write(""" # Image Compressor """)
+uploaded_file = st.file_uploader("Upload your Image to be compressed here", type=".jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image',width=300)
+    st.write("The image is compressed")
+    link = 'http://localhost:8516/media/d27bc46630157c00765b1d806c3f065222818e824ef6cdf1f6d9c84a.jpeg'
+    if st.button('Download'):
+        st.write("To Download the Image right-click on the image and select 'Save Image As' ")
+
+
+
