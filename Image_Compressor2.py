bc6c4873b0c4045205c53067573f6c95c93725a4
Shivam Bhosale <shivambhosale1511@gmail.com>
2021-01-10 18:57:03 +0530

    Update Image_Compressor2.py

--- Image_Compressor2.py
+++ Image_Compressor2.py
@@ -9,7 +9,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image',width=300)
     st.write("The image is compressed")
-    link = 'http://localhost:8516/media/d27bc46630157c00765b1d806c3f065222818e824ef6cdf1f6d9c84a.jpeg'
+    
     if st.button('Download'):
         st.write("To Download the Image right-click on the image and select 'Save Image As' ")
 
